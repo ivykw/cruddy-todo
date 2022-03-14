dd37006a302cc1da6fbd825a5ec0ed8a63af0346
ivyhu <ajianouc@gmail.com>
2022-03-14 15:03:01 -0700

    Complete getNextUniqueId

--- datastore/counter.js
+++ datastore/counter.js
@@ -38,9 +38,16 @@ const writeCounter = (count, callback) => {
 
 // Public API - Fix this function //////////////////////////////////////////////
 
-exports.getNextUniqueId = () => {
-  counter = counter + 1;
-  return zeroPaddedNumber(counter);
+exports.getNextUniqueId = (callback) => {
+  // read the file
+  readCounter((err, counter) =>{
+    counter++;
+    writeCounter(counter, (err, counterString) => {
+      callback(err, counterString);
+    });
+  });
+  // counter = counter + 1;
+  // return zeroPaddedNumber(counter);
 };
 
 
